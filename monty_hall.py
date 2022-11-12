f010a8aedb4aa512e5f4da189489e82c2b83c369
nsilveri <nikis900@gmail.com>
2022-11-12 01:47:28 +0100

    Update monty_hall.py

--- monty_hall.py
+++ monty_hall.py
@@ -1,6 +1,6 @@
 import random
 
-three_vector = [0, 0, 0]
+doors = [0, 0, 0]
 user_choose = -1
 cond_choose = -1
 new_user_choose = -1
@@ -18,36 +18,36 @@ def log_game(string_to_print):
         print(string_to_print)
 
 def winning_box():
-    global three_vector
+    global doors
     right_choose = random.randint(0,2)
-    three_vector[right_choose] = 1
+    doors[right_choose] = 1
 
 def usr_choose():
     global user_choose
     user_choose = random.randint(0,2)
     
 
-def conductor_choose():
+def tv_presenter_choose():
     global cond_choose
     global user_choose
 
     cond_choose = random.randint(0,2)
-    if(three_vector[cond_choose] == 1):
-        conductor_choose()
+    if(doors[cond_choose] == 1):
+        tv_presenter_choose()
     if(cond_choose == user_choose):
-        conductor_choose()
+        tv_presenter_choose()
 
 def and_the_winner_is():
     global user_choose
     global cond_choose
-    global three_vector
+    global doors
     global number_loose
     global number_win
 
-    if(three_vector[user_choose] == 1):
+    if(doors[user_choose] == 1):
         number_win = number_win + 1
         log_game("User WIN!!!")
-    elif(three_vector[user_choose] == 0):
+    elif(doors[user_choose] == 0):
         number_loose = number_loose + 1
         log_game("User LOOSE!!!")
 
@@ -62,11 +62,26 @@ def change_choose():
     new_user_choose = random.randint(0, 2)
     if(new_user_choose == user_choose or new_user_choose == cond_choose):
         change_choose()
-    
+
+def print_door_selected_vector(vector_box):
+    if(vector_box == 0):
+        return "[X][ ][ ]"
+    if(vector_box == 1):
+        return "[ ][X][ ]"
+    if(vector_box == 2):
+        return "[ ][ ][X]"
+
+def print_door_vector(vector_box):
+    if(vector_box == [1, 0, 0]):
+        return "[C][G][G]"
+    if(vector_box == [0, 1, 0]):
+        return "[G][C][G]"
+    if(vector_box == [0, 0, 1]):
+        return "[G][G][C]"
 
 def run_game(game_mode, number_time_run, _log):
     global log_mode
-    global three_vector
+    global doors
     global user_choose
     global new_user_choose
 
@@ -76,24 +91,24 @@ def run_game(game_mode, number_time_run, _log):
     if(number_time_run > 0):
         for run in range(number_time_run):
             log_game("====================")
-            log_game("run: " + str(run + 1))
+            log_game("run: " + str(run + 1) + " | C=Car, G=Goat")
             #set vector to 0, 0, 0
-            three_vector = [0, 0, 0]
+            doors = [0, 0, 0]
             #selecting winning box
             winning_box()
-            log_game("Three vector: " + str(three_vector))
+            log_game("Doors vector:                 " + str(print_door_vector(doors))) #+ str(doors) + " (1 = car, 0 = goat)")
             #user select one box
             usr_choose()
-            log_game("User choose: " + str(user_choose))
+            log_game("User choose a door:           " + str(print_door_selected_vector(user_choose)))# str(user_choose))
             #conductor select one box different from user choose and different from the winning box
-            conductor_choose()
-            log_game("Conductor choose: " + str(cond_choose))
+            tv_presenter_choose()
+            log_game("TV presenter reveals a goat:  " + str(print_door_selected_vector(cond_choose)))
             
             if(game_mode == "change"):
                 #user change his choose after conductor choose
                 change_choose()
                 user_choose = new_user_choose
-                log_game("Final user choose: " + str(user_choose))
+                log_game("User changes the choice to:   " + str(print_door_selected_vector(user_choose)))
             
             if(game_mode == "keep"):
                 #user keep his choose
@@ -103,10 +118,11 @@ def run_game(game_mode, number_time_run, _log):
 
     else:
         print("Number run time [" + str(number_time_run) + "] input not valid")
-    
+
+    print("====================\n")
     print("Monty Hall runned in '" + str(game_mode) + "' mode for " + str(number_time_run) + " times:")
     print("The user won " + str(number_win) + " times [" + str(_map(number_win, 0 , number_time_run, 0, 100)) + "%] and lost " + str(number_loose) + " times [" + str(_map(number_loose, 0 , number_time_run, 0, 100)) + "%]")
 
 
 #first arg["keep" or "change"], second arg[any number run > 0], third arg[1 = log, 0 = no log]
-run_game("change", 10000000, 0)
+run_game("keep", 1000, 1)
